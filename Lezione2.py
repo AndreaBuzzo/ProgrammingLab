397c3f1a7d7c96cf55d68cfdaa4f7fcd0e6fbc8a
AndreaBuzzo <91831622+AndreaBuzzo@users.noreply.github.com>
2021-11-19 14:18:48 +0000

    Lezione 2

---
+++ Lezione2.py
@@ -0,0 +1,11 @@
+mia_lista = [4, 6, 8, 98, 43, 11]
+
+def somma_lista(mia_lista):
+    risultato = 0
+    for i in mia_lista:
+        risultato = risultato + i
+    return risultato
+
+somma = somma_lista(mia_lista)
+print('Risultato: {}'.format(somma))
+      
